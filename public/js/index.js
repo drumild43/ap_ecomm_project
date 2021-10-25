a82ac920d758413e5a96be113499c9deef61f58f
Drumil Deliwala <drumildeliwala@gmail.com>
2021-10-25 21:14:57 +0530

    Initial commit

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+
+gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
+gsap.from(".h1", { opacity: 0, duration: 1, delay: 1.5, x: -200 });
+gsap.from(".img-content h2", { opacity: 0, duration: 1, delay: 2, y: -50 });
+gsap.from(".img-content h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
+gsap.from(".gimg", { opacity: 0, duration: 1, delay: 2.5, x: -45 });
+gsap.from(".img-content a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
+gsap.from(".box", { opacity: 0, duration: 0.5, delay: 1, x: -100 });
