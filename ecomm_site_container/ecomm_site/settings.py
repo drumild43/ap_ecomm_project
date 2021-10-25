f8b88f08f67a458df6b0d857ca82cfbde863f6bc
Drumil Deliwala <drumildeliwala@gmail.com>
2021-10-26 00:23:15 +0530

    Set up store app and run initial migrations

--- ecomm_site_container/ecomm_site/settings.py
+++ ecomm_site_container/ecomm_site/settings.py
@@ -75,11 +75,13 @@ WSGI_APPLICATION = 'ecomm_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'ecom_db',
+        'CLIENT': {
+            'host': 'mongodb+srv://ap_admin:ap_ecom_password@cluster0.nfbmc.mongodb.net/test'
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
