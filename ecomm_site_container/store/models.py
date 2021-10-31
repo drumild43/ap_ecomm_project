f8b43cc4bdf2c8d111c43b679a2fdb072ac2ed4d
Drumil Deliwala <drumildeliwala@gmail.com>
2021-10-31 19:15:24 +0530

    Create models

--- ecomm_site_container/store/models.py
+++ ecomm_site_container/store/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    price = models.PositiveIntegerField()
+
+    """
+    def get_img_name(self):
+        return "img" + str(self.id)
+    """
+    # avg_rating
+    # ^^ average of user ratings; probably computed instead of related to ratings model
+    # review_count
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class EcomUser(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_num = models.CharField(max_length=20)
+    cart = models.OneToOneField('Cart', on_delete=models.SET_NULL, null=True)
+    # password?
+
+class Cart(models.Model):
+    pass
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+
+class Order(models.Model):
+    ordered_on = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(EcomUser, on_delete=models.CASCADE)
+
+    CONFIRMED = 'C'
+    TRANSIT = 'T'
+    DELIVERED = 'D'
+    STATUS_CHOICES = [
+        (CONFIRMED, 'Confirmed'),
+        (TRANSIT, 'In Transit'),
+        (DELIVERED, 'Delivered'),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=CONFIRMED)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+
+"""
+class Review(models.Model):
+    pass
+
+class Rating(models.Model):
+    pass
+"""
