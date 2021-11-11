a53feb255069034773023ccfe4345e7edf18065e
Drumil Deliwala <drumildeliwala@gmail.com>
2021-11-11 18:40:22 +0530

    Improve logout

--- ecomm_site_container/store/urls.py
+++ ecomm_site_container/store/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'store'
 urlpatterns = [
     path('', views.home, name='anon_homepage'),
-    path('<int:user_id>', views.home, name='homepage'),
+    path('<int:user_id>/', views.home, name='homepage'),
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
-    path('logout/<int:user_id>', views.logout, name='logout')
+    path('logout/<int:user_id>/', views.logout, name='logout')
 ]
